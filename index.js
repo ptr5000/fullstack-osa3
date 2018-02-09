3e92ac8520b34fba38bd2309001e2b6b922ba329
ptr5000 <35306800+ptr5000@users.noreply.github.com>
2018-02-09 12:50:11 +0200

    3.6

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let book = [
     {
@@ -24,6 +27,55 @@ let book = [
 
 app.get('/api/persons/', (req, res) => {
     res.json(book)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const b = book.find(entry => entry.id === Number(req.params.id))
+
+    if(b == null) {
+        res.status(404).end()
+    } else {
+        res.json(b) 
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    console.log(body)
+
+    if (body.name === undefined) {
+        return res.status(400).json({error: 'name missing'})
+    }
+
+    if (body.number === undefined) {
+        return res.status(400).json({error: 'number missing'})
+    }
+
+    if(book.find(entry => entry.name == body.name)) {
+        return res.status(400).json({error: 'name already exists'})
+    }
+
+    let n = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 100000000000)
+    }
+
+    book = book.concat(n)
+
+    res.json(n)
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    book = book.filter(entry => entry.id !== Number(req.params.id))
+    res.status(204).end()
+})
+
+
+
+app.get('/info/', (req, res) => {
+    res.send("puhelinluettelossa " + book.length + " henkilön tiedot")
 })
 
 const PORT = 3001
